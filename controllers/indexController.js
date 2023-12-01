2ba159b9c981f8ce2cffe056b9bdda7786b25e7b
ssantiBarrioss <santiago.barrios@hotmail.es>
2023-12-01 03:58:50 -0300

    subo archivos terminados

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,29 +1,50 @@
 const productos= [{
+    id:1,
     nombre:"Carpaccio fresco",
     descripcion:" Entrada Carpaccio de salmón con cítricos",
     precio:65.50 ,
+    imagen:"Carpaccio-de-salmon.jpg",
 },
 {
+    id:2,
     nombre:"Risotto de berenjena",
     descripcion:" Risotto de berenjena y queso de cabra",
     precio:47.00 ,
+    imagen:"Risotto-berenjena-queso-cabra.jpg",
 },
 {
+    id:3,
     nombre:"Mousse de arroz",
     descripcion:"Mousse de arroz con leche y aroma de azahar",
     precio:27.50 ,
+    imagen:"Mousse-de-arroz-con-leche.jpg",
 },
 {
+    id:4,
     nombre:"Espárragos blancos",
     descripcion:"Espárragos blancos con vinagreta de verduras y jamón ibérico",
     precio:37.50 ,
+    imagen:"esparragos.png",
+},
+{
+    id:5,
+    nombre:"Pastel de papas",
+    descripcion:"",
+    precio: 15.00,
+    imagen:"default-ui-image.webp"
 }
 ]
 
 const indexController= {
     index: (req, res) => {
-        res.render("index",{title:"Sal & Pimienta", productos})
+        res.render("index",{title:"Pimienta & Sal", productos})
+    },
+    detail: (req, res) => {
+        const {id}=req.params;
+        const producto= productos.find((producto) => producto.id == id)
+        res.render("detail",{title:producto.nombre, producto})
     }
+
 }
 
 module.exports= indexController
--- routes/index.js
+++ routes/index.js
@@ -3,5 +3,5 @@ const router = express.Router();
 const indexController = require("../controllers/indexController.js")
 /* GET home page. */
 router.get('/', indexController.index);
-
+router.get('/detail/:id', indexController.detail);
 module.exports = router;
